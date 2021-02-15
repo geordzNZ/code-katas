2437474c033faefbdf6f8dda469330289bcfed42
Geordie King <geordie.king@gmail.com>
2021-02-15 22:27:26 +0000

    #2 code kata for 15/2/2021

--- js/2021-02-15.js
+++ js/2021-02-15.js
@@ -1,18 +1,26 @@
-// 5) START===============================================================
+// 1) START===============================================================
 // Title: Printer Errors
 // Link:  https://www.codewars.com/kata/56541980fa08ab47a0000040
 
-function printerError(s) {
-  let errors = 0
+// function printerError(s) {
+//   let errors = 0
 
-  for (let i = 0; i <= s.length; i++){
-    s[i] >= 'n' ? errors ++ : 0
-  }
- console.log(`${errors}/${s.length}`)
+//   for (let i = 0; i <= s.length; i++){
+//     s[i] >= 'n' ? errors ++ : 0
+//   }
+//  console.log(`${errors}/${s.length}`)
 
-}
+// }
+// printerError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz")
+// printerError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmm")
+// 1) END===============================================================
 
-printerError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz")
-printerError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmm")
+// 2) START===============================================================
+// Title: Function 3 - multiplying two numbers
+// https://www.codewars.com/kata/523b66342d0c301ae400003b
 
-// 5) END===============================================================
+// // Write here your multiply-function
+// function multiply(n1,n2){
+//   return n1 * n2
+// }
+// 2) END===============================================================
