847717fd674841e9dd418eee2f98b2dfb21ec392
Geordie King <geordie.king@gmail.com>
2021-02-16 10:31:37 +0000

    #2 kata for 16/Feb/2021

--- js/2021-02-16.js
+++ js/2021-02-16.js
@@ -2,18 +2,35 @@
 // Title: Transportation on vacation
 // https://www.codewars.com/kata/568d0dd208ee69389d000016
 
-function rentalCarCost(d) {
-  return d >= 7 ? (d * 40) - 50 : d >= 3 ? (d * 40) - 20 : (d * 40)
-}
+// function rentalCarCost(d) {
+//   return d >= 7 ? (d * 40) - 50 : d >= 3 ? (d * 40) - 20 : (d * 40)
+// }
 
 // 1) END===============================================================
 
 
 // 2) START===============================================================
-// Title: 
-//
+// Title: Grasshopper - Grade book
+// https://www.codewars.com/kata/55cbd4ba903825f7970000f5
 
-
+// function getGrade (s1, s2, s3) {
+//   let avg = (s1 + s2 + s3)/3
+  
+//   switch(true) {
+//     case (avg >= 90):
+//       return 'A'
+//       break;
+//     case (avg >=80):
+//       return 'B'
+//       break;
+//     case (avg >= 70):
+//       return 'C'
+//     case (avg >= 60):
+//       return 'D'
+//     default:
+//       return 'F'
+//   }
+// }
 
 // 2) END===============================================================
 
