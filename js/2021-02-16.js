6c4af6297836201223d28bef362ace838713c2fd
Geordie King <geordie.king@gmail.com>
2021-02-16 11:37:20 +0000

    #8 kata for 16/Feb/2021

--- js/2021-02-16.js
+++ js/2021-02-16.js
@@ -134,8 +134,12 @@
 
 
 // 8) START===============================================================
-// Title: 
-// 
+// Title: Beginner - Lost Without a Map
+// https://www.codewars.com/kata/57f781872e3d8ca2a000007e
+
+// function maps(x){
+//   return x.map(x => x*=2)
+// }
 
 // 8) END===============================================================
 
