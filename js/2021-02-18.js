ac9f84e4e90090a154a67f326bef8257d1726777
Geordie King <geordie.king@gmail.com>
2021-02-18 16:24:11 +0000

    #6 kata for 18/Feb/2021

--- js/2021-02-18.js
+++ js/2021-02-18.js
@@ -112,8 +112,15 @@
 
 
 // 6) START===============================================================
-// TITLE: 
-// LINK:  
+// TITLE: Find the first non-consecutive number
+// LINK:  https://www.codewars.com/kata/58f8a3a27a5c28d92e000144
+
+// function firstNonConsecutive (arr) {
+//   for (let i = 1; i <= arr.length - 1; i++){
+//     if (Math.abs(arr[i] - arr[i-1]) !== 1){ return arr[i] }
+//   }
+//   return null
+// }
 
 // 6) END===============================================================
 
