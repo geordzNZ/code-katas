9b6ed1c0ad86b059ee8dee34eecdf1c61aaa84b2
Geordie King <geordie.king@gmail.com>
2021-02-18 18:35:30 +0000

    #11 kata for 18/Feb/2021

--- js/2021-02-18.js
+++ js/2021-02-18.js
@@ -195,3 +195,36 @@
 // const index = (array, n) => (n <= 0 || n > array.length-1) ? -1 : array[n]**n
 
 // 10) END===============================================================
+
+
+// 11) START===============================================================
+// TITLE: Who likes it?
+// LINK:  https://www.codewars.com/kata/5266876b8f4bf2da9b000362
+
+// v1
+// function likes(names) {
+//   if (names.length === 0){
+//     return 'no one likes this'
+//   } else if (names.length === 1){
+//     return `${names[0]} likes this`
+//   } else if (names.length === 2){
+//     return `${names[0]} and ${names[1]} like this`
+//   } else if (names.length === 3){
+//     return `${names[0]}, ${names[1]} and ${names[2]} like this`
+//   } else {
+//     return `${names[0]}, ${names[1]} and ${names.length-2} others like this`
+//   }
+// }
+
+// v2
+// const likes = names => {
+//   return (names.length === 0) ? 'no one likes this' :
+//          (names.length === 1) ? `${names[0]} likes this` :
+//          (names.length === 2) ? `${names[0]} and ${names[1]} like this` :
+//          (names.length === 3) ? `${names[0]}, ${names[1]} and ${names[2]} like this` : `${names[0]}, ${names[1]} and ${names.length-2} others like this`
+// }
+
+// 11) END===============================================================
+
+
+
