59a42d54e94a072f582c02c75b282758777a0c1d
Geordie King <geordie.king@gmail.com>
2021-02-21 11:02:11 +0000

    #5 kata for 21/Feb/2021

--- js/2021-02-21.js
+++ js/2021-02-21.js
@@ -85,8 +85,10 @@
 
 
 // 5) START===============================================================
-// TITLE: 
-// LINK:  
+// TITLE: L1: Set Alarm (8 kyu)
+// LINK:  https://www.codewars.com/kata/568dcc3c7f12767a62000038
+
+// const setAlarm = (employed, vacation) => (employed && !vacation) ? true : false
 
 // 5) END===============================================================
 
