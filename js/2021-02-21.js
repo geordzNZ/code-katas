a857196ad52c40d1c5329e763e59e6dcad81f5b9
Geordie King <geordie.king@gmail.com>
2021-02-21 10:55:09 +0000

    #4 kata for 21/Feb/2021

--- js/2021-02-21.js
+++ js/2021-02-21.js
@@ -71,8 +71,15 @@
 
 
 // 4) START===============================================================
-// TITLE: 
-// LINK:  
+// TITLE: Kata Example Twist
+// LINK:  https://www.codewars.com/kata/525c1a07bb6dda6944000031
+
+// // add the value "codewars" to the websites array 1,000 times
+// var websites = []
+// for (let i = 1; i<= 1000;i++) websites.push('codewars')
+
+// Someone elses option
+// var websites = new Array(1000).fill("codewars");
 
 // 4) END===============================================================
 
