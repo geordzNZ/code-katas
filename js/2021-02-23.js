a99d5ccbc9f257124cc41c6280f814de082ec14b
Geordie King <geordie.king@gmail.com>
2021-02-23 14:17:14 +0000

    #8 kata for 23/Feb/2021

--- js/2021-02-23.js
+++ js/2021-02-23.js
@@ -113,8 +113,10 @@
 
 
 // 8) START===============================================================
-// TITLE: 
-// LINK:  
+// TITLE: Exclamation marks series #4: Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string (8 kyu)
+// LINK:  https://www.codewars.com/kata/57faf12b21c84b5ba30001b0
+
+// const remove = (s) => s.replace(/!/g,'').concat('!')
 
 // 8) END===============================================================
 
