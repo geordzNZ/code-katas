5c9c67cf8b0f6d34ba714d71f8a904bb18abfb2f
Geordie King <geordie.king@gmail.com>
2021-02-25 13:10:33 +0000

    #4 kata for 25/Feb/2021

--- js/2021-02-25.js
+++ js/2021-02-25.js
@@ -46,8 +46,10 @@
 
 
 // 4) START===============================================================
-// TITLE: 
-// LINK:  
+// TITLE: Sum Arrays
+// LINK:  https://www.codewars.com/kata/53dc54212259ed3d4f00071c
+
+// const sum = numbers => numbers.reduce((t,c) => t + c,0)
 
 // 4) END===============================================================
 
