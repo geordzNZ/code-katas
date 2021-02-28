bbee03d08361f186273aad6884879707fa00b309
Geordie King <geordie.king@gmail.com>
2021-02-28 19:28:46 +0000

    #5 kata for 28/Feb/2021

--- js/2021-02-28.js
+++ js/2021-02-28.js
@@ -115,8 +115,65 @@
 
 
 // 5) START===============================================================
-// TITLE: 
-// LINK:  
+// TITLE: Get Planet Name By ID
+// LINK:  https://www.codewars.com/kata/515e188a311df01cba000003
+
+// FIX THE BUG
+// function getPlanetName(id){
+//   var name;
+//   switch(id){
+//     case 1:
+//       name = 'Mercury'
+//     case 2:
+//       name = 'Venus'
+//     case 3:
+//       name = 'Earth'
+//     case 4:
+//       name = 'Mars'
+//     case 5:
+//       name = 'Jupiter'
+//     case 6:
+//       name = 'Saturn'
+//     case 7:
+//       name = 'Uranus'
+//     case 8:
+//       name = 'Neptune'
+//   }
+//   return name;
+// }
+
+// MY ANSWER
+// function getPlanetName(id){
+// var name;
+//   switch(id){
+//     case 1:
+//       name = 'Mercury'
+//       break
+//     case 2:
+//       name = 'Venus'
+//       break
+//     case 3:
+//       name = 'Earth'
+//       break
+//     case 4:
+//       name = 'Mars'
+//       break
+//     case 5:
+//       name = 'Jupiter'
+//       break
+//     case 6:
+//       name = 'Saturn'
+//       break
+//     case 7:
+//       name = 'Uranus'
+//       break
+//     case 8:
+//       name = 'Neptune'
+//       break
+//   }
+  
+//   return name;
+// }
 
 // 5) END===============================================================
 
