c1875e8c9d6226dfadb77dd3309df0a6f76c3059
Geordie King <geordie.king@gmail.com>
2021-03-04 15:33:24 +0000

    #5v1 kata for 04/Mar/2021

--- js/2021-03/2021-03-04.js
+++ js/2021-03/2021-03-04.js
@@ -122,8 +122,28 @@
 
 
 // 5) START===============================================================
-// TITLE: 
-// LINK:  
+// TITLE: 101 Dalmatians - squash the bugs, not the dogs!
+// LINK:  https://www.codewars.com/kata/56f6919a6b88de18ff000b36
+
+// FIX THE CODE
+// function howManyDalmations(numer :
+  
+//   var dogs ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIONS!!!"];
+  
+//   var respond = number <= 10 ? dogs[0] (number <= 50 ? dogs[1] : (number = 101  dogs[3] : dogs[2]
+  
+// return respond
+
+// ANSWER v1 -- straight fix
+// function howManyDalmatians(number) {
+  
+//   let dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"];
+  
+//   let respond = (number <= 10) ? dogs[0] : (number <= 50) ? dogs[1] : (number === 101) ? dogs[3] : dogs[2]
+  
+// return respond
+// }
+
 
 // 5) END===============================================================
 
