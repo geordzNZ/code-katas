224bf3375a5bbc523cf0d1bcf7ece10789f72d88
Geordie King <geordie.king@gmail.com>
2021-03-04 15:13:53 +0000

    #4v2 kata for 04/Mar/2021

--- js/2021-03/2021-03-04.js
+++ js/2021-03/2021-03-04.js
@@ -107,6 +107,17 @@
 //   return longest
 // }
 
+// ANSWER v2
+// function findLongest(str) {
+//   let spl = str.split(" ")
+//   let longest = 0
+  
+//   for (var i = 0; i < spl.length; i++) {
+//     if (spl[i].length > longest) longest = spl[i].length
+//   }
+//   return longest
+// }
+
 // 4) END===============================================================
 
 
