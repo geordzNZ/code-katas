b7591c76a234ec25cf8885798a8b1ca67fd9a7cf
Geordie King <geordie.king@gmail.com>
2021-03-06 20:16:29 +0000

    #5 kata for 06/Mar/2021

--- js/2021-03/2021-03-06.js
+++ js/2021-03/2021-03-06.js
@@ -50,6 +50,8 @@
 // TITLE: Disemvowel Trolls
 // LINK:  https://www.codewars.com/kata/52fba66badcd10859f00097e
 
+// const disemvowel = str => str.replace(/[aeiou]/gi,'')
+
 // 5) END===============================================================
 
 
