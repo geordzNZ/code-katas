c1edb8d1e09cdb8c9d0f88236bf8256b589e822e
Geordie King <geordie.king@gmail.com>
2021-03-10 18:55:20 +0000

    #5 kata for 10/Mar/2021

--- js/2021-03/2021-03-10.js
+++ js/2021-03/2021-03-10.js
@@ -59,8 +59,10 @@
 
 
 // 5) START===============================================================
-// TITLE: 
-// LINK:  
+// TITLE: Remove First and Last Character
+// LINK:  https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0
+
+// const removeChar = str => str.substr(1,str.length-2)
 
 // 5) END===============================================================
 
