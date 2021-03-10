6bc4db97aeb1de5053a68c5954233f03e7a3ba9d
Geordie King <geordie.king@gmail.com>
2021-03-10 18:45:18 +0000

    #3 kata for 10/Mar/2021

--- js/2021-03/2021-03-10.js
+++ js/2021-03/2021-03-10.js
@@ -33,8 +33,15 @@
 
 
 // 3) START===============================================================
-// TITLE: 
-// LINK:  
+// TITLE: Grasshopper - Messi Goals
+// LINK:  https://www.codewars.com/kata/55ca77fa094a2af31f00002a
+
+// var laLigaGoals = 43
+// var championsLeagueGoals = 10
+// var copaDelReyGoals = 5
+// var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
+
+// var totalGoals
 
 // 3) END===============================================================
 
