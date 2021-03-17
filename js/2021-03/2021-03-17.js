467836c85566b0ddbbc33ac7811e468ec68508e9
Geordie King <geordie.king@gmail.com>
2021-03-17 23:58:56 +0000

    #5 kata for 17/Mar/2021

--- js/2021-03/2021-03-17.js
+++ js/2021-03/2021-03-17.js
@@ -78,7 +78,9 @@
 // TITLE: Even or Odd (8 kyu)
 // LINK:  https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
 
-
+// function even_or_odd(number) {
+//   return number%2===0 ? 'Even' : 'Odd'
+// }
 
 // 5) END===============================================================
 
