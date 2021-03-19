a06a09a265e9f00c1593ec2cf99be8923eec6b7e
Geordie King <geordie.king@gmail.com>
2021-03-19 22:36:35 +0000

    #3 kata for 19/Mar/2021

--- js/2021-03/2021-03-19.js
+++ js/2021-03/2021-03-19.js
@@ -35,8 +35,12 @@
 
 
 // 3) START===============================================================
-// TITLE: 
-// LINK:  
+// TITLE: Convert a string to an array (8 kyu)
+// LINK:  https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+
+// function stringToArray(string){
+//   return string.split(' ')
+// }
 
 // 3) END===============================================================
 
