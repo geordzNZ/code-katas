62ae073a1f3905f3fd830c57509690bb4dbbcfa4
Geordie King <geordie.king@gmail.com>
2021-03-20 21:46:48 +0000

    #7v2 kata for 20/Mar/2021

--- js/2021-03/2021-03-20.js
+++ js/2021-03/2021-03-20.js
@@ -90,6 +90,25 @@
 //   return ans[nbPetals % 6]
 // }
 
+// v2
+// function howMuchILoveYou(nbPetals) {
+  
+//   switch (nbPetals % 6){
+//     case 1:
+//       return 'I love you'
+//     case 2:
+//       return 'a little'
+//     case 3:
+//       return 'a lot'
+//     case 4:
+//       return 'passionately'
+//     case 5:
+//       return 'madly'
+//     case 0:
+//       return 'not at all'
+//   }
+// }
+
 // 7) END===============================================================
 
 
