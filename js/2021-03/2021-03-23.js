63eeded57583d2f727439dbc804a73e3792f533d
Geordie King <geordie.king@gmail.com>
2021-03-23 00:33:52 +0000

    #3 kata for 23/Mar/2021

--- js/2021-03/2021-03-23.js
+++ js/2021-03/2021-03-23.js
@@ -50,8 +50,14 @@
 
 
 // 3) START===============================================================
-// TITLE: 
-// LINK:  
+// TITLE: Volume of a Cuboid (8 kyu)
+// LINK:  https://www.codewars.com/kata/58261acb22be6e2ed800003a
+
+// class Kata {
+//   static getVolumeOfCuboid(l, w, h) {
+//    return l*w*h
+//   }
+// }
 
 // 3) END===============================================================
 
