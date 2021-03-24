093e1cda1c13fdbf52651a5696a3ab42994ab375
Geordie King <geordie.king@gmail.com>
2021-03-24 19:11:58 +0000

    #3v1 kata for 24/Mar/2021

--- js/2021-03/2021-03-24.js
+++ js/2021-03/2021-03-24.js
@@ -32,6 +32,33 @@
 // TITLE: Alan Partridge I - Partridge Watch (8 kyu)
 // LINK:  https://www.codewars.com/kata/5808c8eff0ed4210de000008
 
+// v1 - Code works for basic tests, but fails for full ... tests look wrong to me???
+// function part(x){
+
+//   let sW = ['Partridge','PearTree','Chat','Dan','Toblerone','Lynn','AlphaPapa','Nomad']
+//   let fW = []
+
+//   console.log('x:  '+x)
+//   console.log('')
+
+//   for (let i=0; i<sW.length; i++){
+//     for (let j=0; j<x.length; j++){
+//       console.log(`i: ${i} - ${sW[i]}   x: ${j} - ${x[j]}`)
+//       if (sW[i] === x[j]){
+        
+//         fW.push(sW[i])
+//         console.log(`  f: ${fW}`)
+//         x.splice(j,1)
+//         console.log(`  x: ${x}`)
+//         j--
+        
+//       }
+//     }
+//   }
+  
+//    console.log('f:  '+fW)
+//    return fW.length>0 ? `Mine's a Pint${'!'.repeat(fW.length)}` : 'Lynn, I\'ve pierced my foot on a spike!!' 
+// }
 
 
 // 3) END===============================================================
