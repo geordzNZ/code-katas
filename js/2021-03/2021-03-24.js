3d85fab544e4caded21583b62455706e55c6bbd2
Geordie King <geordie.king@gmail.com>
2021-03-24 20:11:30 +0000

    #8 kata for 24/Mar/2021

--- js/2021-03/2021-03-24.js
+++ js/2021-03/2021-03-24.js
@@ -128,8 +128,12 @@
 
 
 // 8) START===============================================================
-// TITLE: 
-// LINK:  
+// TITLE: Blood-Alcohol Content ( 8 kyu)
+// LINK:  https://www.codewars.com/kata/571b6a4a7beb0a8ade0007a8
+
+// function bloodAlcoholContent(drinks, weight, sex, time){
+//   return Number(((drinks.ounces * drinks.abv * 5.14 / weight * (sex==='male' ? 0.73 : 0.66)) - 0.015 * time).toFixed(4))
+// }
 
 // 8) END===============================================================
 
