6549928c4e31480bf1d6762f3ca583493651ae46
Geordie King <geordie.king@gmail.com>
2021-03-25 21:47:21 +0000

    #2 kata for 25/Mar/2021

--- js/2021-03/2021-03-25.js
+++ js/2021-03/2021-03-25.js
@@ -22,7 +22,9 @@
 // TITLE: Is n divisible by x and y? (8 kyu)
 // LINK:  https://www.codewars.com/kata/5545f109004975ea66000086
 
-
+// function isDivisible(n, x, y) {
+//   return (n%x===0 && n%y===0)
+// }
 
 // 2) END===============================================================
 
