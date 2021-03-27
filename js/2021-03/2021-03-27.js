92e188c355b4f5d38968a330cbd224ae1fb973b0
Geordie King <geordie.king@gmail.com>
2021-03-27 20:33:35 +0000

    #1 kata for 27/Mar/2021

--- js/2021-03/2021-03-27.js
+++ js/2021-03/2021-03-27.js
@@ -10,7 +10,9 @@
 // TITLE: Template Strings (8 kyu)
 // LINK:  https://www.codewars.com/kata/55a14f75ceda999ced000048
 
-
+// var TempleStrings = function(obj, feature) {
+//   return `${obj} are ${feature}`
+// }
 
 // 1) END===============================================================
 
