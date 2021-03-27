d97b4c1afb89c6b92e375dcf3a1ef07d1e576197
Geordie King <geordie.king@gmail.com>
2021-03-27 23:15:54 +0000

    #5 kata for 27/Mar/2021

--- js/2021-03/2021-03-27.js
+++ js/2021-03/2021-03-27.js
@@ -55,6 +55,11 @@
 // TITLE: How good are you really? (8 kyu)
 // LINK:  https://www.codewars.com/kata/5601409514fc93442500010b
 
+// function betterThanAverage(classPoints, yourPoints) {
+//   return yourPoints > (classPoints.reduce((t,v) => t+=v)/classPoints.length)
+// }
+
+
 // 5) END===============================================================
 
 
