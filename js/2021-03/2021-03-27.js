5e6c44d1be08cf361b57f597a9e3095d76875916
Geordie King <geordie.king@gmail.com>
2021-03-27 21:00:29 +0000

    #3 kata for 27/Mar/2021

--- js/2021-03/2021-03-27.js
+++ js/2021-03/2021-03-27.js
@@ -35,7 +35,9 @@
 // TITLE: altERnaTIng cAsE <=> ALTerNAtiNG CaSe (8 kyu)
 // LINK:  https://www.codewars.com/kata/56efc695740d30f963000557
 
-
+// String.prototype.toAlternatingCase = function () {
+//   return this.split("").map(a => a === a.toUpperCase()? a.toLowerCase(): a.toUpperCase()).join('')
+// }
 
 // 3) END===============================================================
 
