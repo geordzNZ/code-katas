dcdbcee9fc835338cb192e6c9b48181450adc162
Geordie King <geordie.king@gmail.com>
2021-04-03 22:10:40 +0100

    #1v2 kata for 03/Apr/2021

--- js/2021-04/2021-04-03.js
+++ js/2021-04/2021-04-03.js
@@ -10,10 +10,19 @@
 // TITLE: Who ate the cookie? (8 kyu)
 // LINK:  https://www.codewars.com/kata/55a996e0e8520afab9000055
 
+// v1
 // function cookie(x){
 //   return (typeof x === 'string') ? "Who ate the last cookie? It was Zach!" :
 //          (typeof x === 'number') ? "Who ate the last cookie? It was Monica!" : 
 //                                    "Who ate the last cookie? It was the dog!"
+// }
+
+// v2
+// function cookie(x){
+//   let msg = 'Who ate the last cookie? It was '
+//   let name = typeof x==='string' ? 'Zach!' : typeof x==='number' ? 'Monica!' : 'the dog!'
+  
+//   return msg + name
 // }
 
 // 1) END===============================================================
