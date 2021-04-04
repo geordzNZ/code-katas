8c3a959550e35110edce23f92dd7a6b4669d6144
Geordie King <geordie.king@gmail.com>
2021-04-04 22:32:50 +0100

    remove 4/5/6 from 2021-04-03.js (wrongly input)

--- js/2021-04/2021-04-03.js
+++ js/2021-04/2021-04-03.js
@@ -59,48 +59,22 @@
 
 
 // 4) START===============================================================
-// TITLE: Geometry Basics: Triangle Area in 2D (6 kyu)
-// LINK:  https://www.codewars.com/kata/58e4377c46e371aee7001262/train/javascript
-
-// function triangleArea(triangle){
-  
-//   let hyp1 = Math.hypot(triangle.a.x-triangle.b.x,triangle.a.y-triangle.b.y)
-//   let hyp2 = Math.hypot(triangle.b.x-triangle.c.x,triangle.b.y-triangle.c.y)
-//   let hyp3 = Math.hypot(triangle.c.x-triangle.a.x,triangle.c.y-triangle.a.y)
-  
-//   return 0.25 * Math.sqrt((hyp1 + hyp2 + hyp3) * (-hyp1 + hyp2 + hyp3) * (hyp1 - hyp2 + hyp3) * (hyp1 + hyp2 - hyp3)) 
-// }
+// TITLE: 
+// LINK:  
 
 // 4) END===============================================================
 
 
 // 5) START===============================================================
-// TITLE: Area of a Circle (7 kyu)
-// LINK:  https://www.codewars.com/kata/537baa6f8f4b300b5900106c
-
-// var circleArea = r => r>0 ? +(Math.PI*Math.pow(r,2)).toFixed(2) : false
+// TITLE: 
+// LINK:  
 
 // 5) END===============================================================
 
 
 // 6) START===============================================================
-// TITLE: Complementary DNA (7 kyu)
-// LINK:  https://www.codewars.com/kata/554e4a2f232cdd87d9000038
-
-// v1
-// function DNAStrand(dna){
-//   let splitDNAarr = dna.split('')
-//   let newDNAarr = splitDNAarr.map((char) => {
-//     return char === 'A' ? 'T' : 
-//            char === 'T' ? 'A' :
-//            char === 'C' ? 'G' : 'C'
-//   })
-  
-//   return newDNAarr.join('')
-// }
-
-// v2
-// const DNAStrand = dna => dna.split('').map((char) => char === 'A' ? 'T' : char === 'T' ? 'A' : char === 'C' ? 'G' : 'C').join('')
+// TITLE: 
+// LINK:  
 
 // 6) END===============================================================
 
