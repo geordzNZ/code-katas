7760493cc639286747a557eccf273f869742ba9c
Geordie King <geordie.king@gmail.com>
2021-04-11 21:25:52 +0100

    #2v2 kata for 11/Apr/2021

--- js/2021-04/2021-04-11.js
+++ js/2021-04/2021-04-11.js
@@ -44,6 +44,13 @@
 //   return sum.toFixed(2)
 // }
 
+// v2
+// function SeriesSum(n) {
+//   let sum = 0
+//   for (let i=0; i<n; i++){ sum+= 1/(i*3+1) }
+//   return sum.toFixed(2)
+// }
+
 // 2) END===============================================================
 
 
