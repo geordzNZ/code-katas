c45f9e22d89e0996354a88d95ffc578266f335e8
Geordie King <geordie.king@gmail.com>
2021-04-13 22:37:27 +0100

    #4 kata for 13/Apr/2021

--- js/2021-04/2021-04-13.js
+++ js/2021-04/2021-04-13.js
@@ -44,6 +44,8 @@
 // TITLE: All Star Code Challenge #1 97 kyu
 // LINK:  https://www.codewars.com/kata/5863f97fb3a675d9a700003f
 
+// const sumPPG =(p1,p2)=> p1.ppg + p2.ppg
+
 // 4) END===============================================================
 
 
