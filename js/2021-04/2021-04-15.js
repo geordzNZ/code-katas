99e68f1376b61d46b034d153bdab3262cc47c235
Geordie King <geordie.king@gmail.com>
2021-04-15 21:21:13 +0100

    #2 kata for 15/Apr/2021

--- js/2021-04/2021-04-15.js
+++ js/2021-04/2021-04-15.js
@@ -21,6 +21,8 @@
 // TITLE: JavaScript Array Filter (7 kyu)
 // LINK:  https://www.codewars.com/kata/514a6336889283a3d2000001
 
+// const getEvenNumbers =numbersArray=> numbersArray.filter(n=>n%2===0) 
+
 // 2) END===============================================================
 
 
