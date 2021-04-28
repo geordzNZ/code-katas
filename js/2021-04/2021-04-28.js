546969ca1265aaf0d23aeb13c4c10383b2a524eb
Geordie King <geordie.king@gmail.com>
2021-04-28 19:35:24 +0100

    #1-3 kata for 28/Apr/2021

--- js/2021-04/2021-04-28.js
+++ js/2021-04/2021-04-28.js
@@ -7,22 +7,62 @@
 //   Updates to js file = comment = git commit -am "#X kata for 28/Apr/2021"
 
 // 1) START===============================================================
-// TITLE: 
-// LINK:  
+// TITLE: Ninja vs Samurai: Strike (7 kyu)
+// LINK:  https://www.codewars.com/kata/517b0f33cd023d848d000001
+
+// v1
+// class Warrior {
+//   constructor(name) {
+//     this.name = name;
+//     this.health = 100;
+//   }
+  
+//   strike(enemy, swings) {
+//     enemy.health = Math.max(0, enemy.health - (swings * 10));   
+//   }
+// }
+
+// v2
+// var Warrior = function(name){
+//   this.name = name;  
+//   this.health = 100;
+
+// this.strike = function(enemy, swings) {
+//   enemy.health = Math.max(0, enemy.health - (swings * 10));   
+// }
+// }
 
 // 1) END===============================================================
 
 
 // 2) START===============================================================
-// TITLE: 
-// LINK:  
+// TITLE: Mean vs. Median (7 kyu)
+// LINK:  https://www.codewars.com/kata/5806445c3f1f9c2f72000031
+
+// function meanVsMedian(numbers) {
+//   let mean = numbers.reduce((t,v)=>t+=v,0)/numbers.length
+//   let median = (numbers.sort((a,b)=>a-b))[Math.floor(numbers.length/2)]
+  
+//   return (mean > median) ? 'mean' : (mean < median) ? 'median' : 'same'
+// }
 
 // 2) END===============================================================
 
 
 // 3) START===============================================================
-// TITLE: 
-// LINK:  
+// TITLE: Tube strike options calculator (7 kyu)
+// LINK:  https://www.codewars.com/kata/568ade64cfd7a55d9300003e
+
+// function calculator(distance, busDrive, busWalk)  {
+//   let walkD = distance / walk
+//   let busD = busDrive / bus + busWalk / walk
+  
+//   return walkD > 2 ? 'Bus' : 
+//          walkD <= 0.12 ? 'Walk' : 
+//          walkD === busD ? 'Walk' :
+//          walkD > busD ? 'Bus' : 'Walk'
+  
+// }
 
 // 3) END===============================================================
 
