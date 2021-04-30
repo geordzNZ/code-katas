f8d840b7a99a0988c16a26445d37aa6a7e6b1988
Geordie King <geordie.king@gmail.com>
2021-04-30 20:17:21 +0100

    #3 kata for 30/Apr/2021

--- js/2021-04/2021-04-30.js
+++ js/2021-04/2021-04-30.js
@@ -26,8 +26,10 @@
 
 
 // 3) START===============================================================
-// TITLE: 
-// LINK:  
+// TITLE: No Loops 1 - Small enough? (7 kyu)
+// LINK:  https://www.codewars.com/kata/57cc4853fa9fc57a6a0002c2
+
+// const smallEnough =(a,limit)=> a.filter((v)=>v<=limit).length===a.length
 
 // 3) END===============================================================
 
