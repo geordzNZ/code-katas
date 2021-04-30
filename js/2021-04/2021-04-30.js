1f72bb82bb077cf345899c7b3e9bb7a169cec918
Geordie King <geordie.king@gmail.com>
2021-04-30 20:09:01 +0100

    #1X kata for 30/Apr/2021

--- js/2021-04/2021-04-30.js
+++ js/2021-04/2021-04-30.js
@@ -7,8 +7,10 @@
 //   Updates to js file = comment = git commit -am "#X kata for 30/Apr/2021"
 
 // 1) START===============================================================
-// TITLE: 
-// LINK:  
+// TITLE: [Code Golf] Return Odd No Matter What (7 kyu)
+// LINK:  https://www.codewars.com/kata/5f882dcc272e7a00287743f5
+
+// alwaysOdd=n=>n%2?n:n-1
 
 // 1) END===============================================================
 
