25d515a48b23764bab8106a2763085bafab69e7e
Geordie King <geordie.king@gmail.com>
2021-05-05 14:27:08 +0100

    #1 kata for 05/May/2021

--- js/2021-05/2021-05-05.js
+++ js/2021-05/2021-05-05.js
@@ -10,6 +10,8 @@
 // TITLE: Keep up the hoop (8 kyu)
 // LINK:  https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145
 
+// const hoopCount =n=> n>=10 ?  "Great, now move on to tricks" : "Keep at it until you get it"
+
 // 1) END===============================================================
 
 
