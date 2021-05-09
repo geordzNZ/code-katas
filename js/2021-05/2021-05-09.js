2f3ef6bdbe77a710d71f5e08ee5cf0ed362a1558
Geordie King <geordie.king@gmail.com>
2021-05-09 13:46:29 +0100

    #2v2 kata for 09/May/2021

--- js/2021-05/2021-05-09.js
+++ js/2021-05/2021-05-09.js
@@ -27,6 +27,9 @@
 //   return false
 // }
 
+// v2
+// const timeForMilkAndCookies =date=> (date.getMonth()===11 && date.getDate()===24)
+
 // 2) END===============================================================
 
 
