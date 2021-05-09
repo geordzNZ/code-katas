82c7cc17112e205bc710fd82c9db77723ff158c2
Geordie King <geordie.king@gmail.com>
2021-05-09 12:50:52 +0100

    #1 kata for 09/May/2021

--- js/2021-05/2021-05-09.js
+++ js/2021-05/2021-05-09.js
@@ -8,6 +8,8 @@
 // TITLE: Sort the Gift Code (7 kyu)
 // LINK:  https://www.codewars.com/kata/52aeb2f3ad0e952f560005d3
 
+// const sortGiftCode =code=> code.split('').sort().join('')
+
 // 1) END===============================================================
 
 
