821d3b78c697b2cc1de66c92761da1445ff3c6bd
Geordie King <geordie.king@gmail.com>
2021-05-19 00:47:05 +0100

    #3v2 kata for 19/May/2021

--- js/2021-05/2021-05-19.js
+++ js/2021-05/2021-05-19.js
@@ -53,6 +53,16 @@
   
 //   return 'Unknown'
 // }
+
+// v2
+// function getIssuer(number) {
+//   var s = number.toString();
+//   if (/^3[4|7]\d{13}$/.test(s)) return "AMEX";
+//   if (/^6011\d{12}$/.test(s)) return "Discover";
+//   if (/^5[1-5]\d{14}$/.test(s)) return "Mastercard";
+//   if (/^4(\d{12}|\d{15})$/.test(s)) return "VISA";
+//   return "Unknown";
+// }
 // 3) END===============================================================
 
 
