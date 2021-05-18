e4ae78216044168d2423b4d6781bf308f9bb410b
Geordie King <geordie.king@gmail.com>
2021-05-18 20:28:49 +0100

    #2 kata for 18/May/2021

--- js/2021-05/2021-05-18.js
+++ js/2021-05/2021-05-18.js
@@ -22,6 +22,10 @@
 // TITLE: Last (7 kyu)
 // LINK:  https://www.codewars.com/kata/541629460b198da04e000bb9
 
+// function last(list){
+//   var last = arguments[arguments.length - 1];
+//   return last[last.length - 1] || last;
+// }
 // 2) END===============================================================
 
 
