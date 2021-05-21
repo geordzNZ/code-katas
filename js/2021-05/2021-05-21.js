a76426625a60414782e3d0f22fc2ca2fc09673f5
Geordie King <geordie.king@gmail.com>
2021-05-21 02:21:10 +0100

    #2v2 kata for 21/May/2021

--- js/2021-05/2021-05-21.js
+++ js/2021-05/2021-05-21.js
@@ -45,6 +45,11 @@
   
 //   return b && i && n && g && o ? 'WIN' : 'LOSE'
 // }
+
+// v2
+// function bingo(a) {  
+//   return a.includes(2) && a.includes(9) && a.includes(14) && a.includes(7) && a.includes(15) ? 'WIN' : 'LOSE'
+// }
 // 2) END===============================================================
 
 
