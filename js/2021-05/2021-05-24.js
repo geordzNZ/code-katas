4e27bf1512c53175ccc8a0fb770c8a25cd1a3c3c
Geordie King <geordie.king@gmail.com>
2021-05-24 21:53:45 +0100

    #3 kata for 24/May/2021

--- js/2021-05/2021-05-24.js
+++ js/2021-05/2021-05-24.js
@@ -35,6 +35,12 @@
 // TITLE: A Strange Trip to the Market (8 kyu)
 // LINK:  https://www.codewars.com/kata/55ccdf1512938ce3ac000056
 
+// function isLockNessMonster(s) {
+//   return s.includes('tree fiddy') || 
+//          s.includes('3.50') || 
+//          s.includes('three fifty')
+// }
+
 // 3) END===============================================================
 
 
