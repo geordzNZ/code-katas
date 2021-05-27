87db1c5bb412d88fb5a02f204d9ab1a803577b88
Geordie King <geordie.king@gmail.com>
2021-05-27 21:39:03 +0100

    #1v2 kata for 27/May/2021

--- js/2021-05/2021-05-27.js
+++ js/2021-05/2021-05-27.js
@@ -15,6 +15,13 @@
 //   let scoreY = y.split('').reduce((t,c) => t+=ltrs[c],0)
 //   return scoreX > scoreY ? x : scoreY > scoreX ? y : 'Tie!'
 // }
+
+// v2
+// function battle(x, y) {
+//   let ltrs = {A:1,B:2,C:3,D:4,E:5,F:6,G:7,H:8,I:9,J:10,K:11,L:12,M:13,N:14,O:15,P:16,Q:17,R:18,S:19,T:20,U:21,V:22,W:23,X:24,Y:25,Z:26}
+//   let score = x.split('').reduce((t,c) => t+=ltrs[c],0) - y.split('').reduce((t,c) => t+=ltrs[c],0)
+//   return score > 0 ? x : score < 0 ? y : 'Tie!'
+// }
 // 1) END===============================================================
 
 
