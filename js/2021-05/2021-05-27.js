24cb29b8c8dd574bb8b83779a93704a1f29ba346
Geordie King <geordie.king@gmail.com>
2021-05-27 21:37:30 +0100

    #1v1 kata for 27/May/2021

--- js/2021-05/2021-05-27.js
+++ js/2021-05/2021-05-27.js
@@ -8,6 +8,13 @@
 // TITLE: Battle of the characters (Easy) (7 kyu)
 // LINK:  https://www.codewars.com/kata/595519279be6c575b5000016
 
+// v1
+// function battle(x, y) {
+//   let ltrs = {A:1,B:2,C:3,D:4,E:5,F:6,G:7,H:8,I:9,J:10,K:11,L:12,M:13,N:14,O:15,P:16,Q:17,R:18,S:19,T:20,U:21,V:22,W:23,X:24,Y:25,Z:26}
+//   let scoreX = x.split('').reduce((t,c) => t+=ltrs[c],0)
+//   let scoreY = y.split('').reduce((t,c) => t+=ltrs[c],0)
+//   return scoreX > scoreY ? x : scoreY > scoreX ? y : 'Tie!'
+// }
 // 1) END===============================================================
 
 
