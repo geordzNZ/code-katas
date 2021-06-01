3a08993ecd7952db60b532e26bbcf3ceb52371f7
Geordie King <geordie.king@gmail.com>
2021-06-01 18:34:30 +0100

    #1v1 kata for 01/Jun/2021

--- js/2021-06/2021-06-01.js
+++ js/2021-06/2021-06-01.js
@@ -8,6 +8,15 @@
 // TITLE: Growth of a Population (7 kyu)
 // LINK:  https://www.codewars.com/kata/563b662a59afc2b5120000c6
 
+// v1
+// function nbYear(p0, percent, aug, p) {
+//   let i = 0
+//   while (p0 < p){
+//     p0 = p0 + (p0 *(percent/100)) + aug
+//     i++
+//   }
+//   return i
+// }
 // 1) END===============================================================
 
 
