f51d3052a2618de2f4eaa59a8925ef61275bbf16
Geordie King <geordie.king@gmail.com>
2021-06-04 00:31:45 +0100

    #1 kata for 04/Jun/2021

--- js/_template 2021-MM-DD.js
+++ js/_template 2021-MM-DD.js
@@ -21,6 +21,20 @@
 //   }
 //   return result;
 // }
+
+// the fix
+// function evalObject(value){
+//   var result = 0;
+//   switch(value.operation){
+//     case'+': result = value.a + value.b; break;
+//     case'-': result = value.a - value.b; break;
+//     case'/': result = value.a / value.b; break;
+//     case'*': result = value.a * value.b; break;
+//     case'%': result = value.a % value.b; break;
+//     case'^': result = Math.pow(value.a, value.b); break;
+//   }
+//   return result;
+// }
 // 1) END===============================================================
 
 
