dc60034d4ac617bfdc0f5ddc95bed871104bbbdf
Geordie King <geordie.king@gmail.com>
2021-06-03 19:23:03 +0100

    #1-fix-v1 kata for 03/Jun/2021

--- js/2021-06/2021-06-03.js
+++ js/2021-06/2021-06-03.js
@@ -19,6 +19,18 @@
 // 			return 'Full-Stack Developer'
 // 		}
 // yourFutureCareer();
+
+// my fix v1
+// function yourFutureCareer(){
+// 	let career = Math.random()
+// 	if (career <= 0.32) { 
+//     return 'FrontEnd Developer' 
+//   } else if (career <= 0.65) { 
+//     return 'BackEnd Developer' 
+//   }	else { 
+//     return 'Full-Stack Developer' 
+//   }
+// }
 // 1) END===============================================================
 
 
