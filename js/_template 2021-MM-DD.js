af94aa621ee113cb1d5342b264d49a9a0425d7cf
Geordie King <geordie.king@gmail.com>
2021-06-04 01:04:24 +0100

    #3 kata for 04/Jun/2021

--- js/_template 2021-MM-DD.js
+++ js/_template 2021-MM-DD.js
@@ -69,9 +69,14 @@
 
 
 // 3) START===============================================================
-// TITLE: 
-// LINK:  
+// TITLE: Largest pair sum in array (7 kyu)
+// LINK:  https://www.codewars.com/kata/556196a6091a7e7f58000018
 
+//v1
+// function largestPairSum(numbers){
+//   numbers.sort((a,b)=>b-a)
+//   return numbers[0] + numbers[1] 
+// }
 // 3) END===============================================================
 
 
