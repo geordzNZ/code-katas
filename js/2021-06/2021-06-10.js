d3271dbb319d4fd189d6c3a2616635d2b003558f
Geordie King <geordie.king@gmail.com>
2021-06-10 21:10:05 +0100

    #3v1 kata for 10/Jun/2021

--- js/2021-06/2021-06-10.js
+++ js/2021-06/2021-06-10.js
@@ -34,6 +34,8 @@
 // TITLE: Be Concise I - The Ternary Operator (8 kyu)
 // LINK:  https://www.codewars.com/kata/56f3f6a82010832b02000f38
 
+// v1
+// const describeAge=a=> a<=12 ? "You're a(n) kid" : a<=17 ? "You're a(n) teenager" : a<=64 ? "You're a(n) adult" : "You're a(n) elderly"
 // 3) END===============================================================
 
 
