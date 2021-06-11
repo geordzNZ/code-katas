6deecaaa9962f701c9bc81b536e0c82efeef16ff
Geordie King <geordie.king@gmail.com>
2021-06-11 14:45:02 +0100

    #3v2 kata for 11/Jun/2021

--- js/2021-06/2021-06-11.js
+++ js/2021-06/2021-06-11.js
@@ -60,6 +60,17 @@
 //     default: return "Wrong, please enter a number between 1 and 7";break;
 //   }
 // }
+
+// v2
+// function whatday(num) { 
+//   return num===1 ? "Sunday" :
+//          num===2 ? "Monday" :
+//          num===3 ? "Tuesday" :
+//          num===4 ? "Wednesday" :
+//          num===5 ? "Thursday" :
+//          num===6 ? "Friday" :
+//          num===7 ? "Saturday" : "Wrong, please enter a number between 1 and 7"
+// }
 // 3) END===============================================================
 
 
