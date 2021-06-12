0ab0ac37ab93b31d502e0e43545bcd198e75304e
Geordie King <geordie.king@gmail.com>
2021-06-12 15:33:00 +0100

    #2v1 kata for 12/Jun/2021

--- js/2021-06/2021-06-12.js
+++ js/2021-06/2021-06-12.js
@@ -24,6 +24,13 @@
 // TITLE: Student's Final Grade (7 kyu)
 // LINK:  https://www.codewars.com/kata/5ad0d8356165e63c140014d4
 
+// v1
+// function finalGrade (exam, projects) {
+//   if (exam>90 || projects>10) return 100
+//   if (exam>75 && projects>=5) return 90
+//   if (exam>50 && projects>=2) return 75
+//   return 0
+// }
 // 2) END===============================================================
 
 
