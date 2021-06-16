eedf64a4599057c9046c3bc8f4698ac1aa2b60b8
Geordie King <geordie.king@gmail.com>
2021-06-16 18:50:22 +0100

    #4v1 kata for 16/Jun/2021

--- js/2021-06/2021-06-16.js
+++ js/2021-06/2021-06-16.js
@@ -57,6 +57,25 @@
 // TITLE: Training JS #8: Conditional statement--switch (8 kyu)
 // LINK:  https://www.codewars.com/kata/572059afc2f4612825000d8a
 
+// v1
+// function howManydays(month){
+//   let days;
+//   switch (month){
+//     case 1: days = 31;break;
+//     case 2: days = 28;break;
+//     case 3: days = 31;break;
+//     case 4: days = 30;break;
+//     case 5: days = 31;break;
+//     case 6: days = 30;break;
+//     case 7: days = 31;break;
+//     case 8: days = 31;break;
+//     case 9: days = 30;break;
+//     case 10: days = 31;break;
+//     case 11: days = 30;break;
+//     case 12: days = 31;break;
+//   }
+//   return days;
+// }
 // 4) END===============================================================
 
 
