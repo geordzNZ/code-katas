8296e00959f9f177b607ba1ab6c9ffc4d501be5b
Geordie King <geordie.king@gmail.com>
2021-06-16 18:49:26 +0100

    #3v2 kata for 16/Jun/2021

--- js/2021-06/2021-06-16.js
+++ js/2021-06/2021-06-16.js
@@ -44,6 +44,12 @@
 //   }
 //   return res
 // }
+
+// v2
+// function toLeetSpeak(str) {
+//   let alphabet = {A:'@',B:'8',C:'(',D:'D',E:'3',F:'F',G:'6',H:'#',I:'!',J:'J',K:'K',L:'1',M:'M',N:'N',O:'0',P:'P',Q:'Q',R:'R',S:'$',T:'7',U:'U',V:'V',W:'W',X:'X',Y:'Y',Z:'2'}
+//   return [...str].reduce((res,s)=> /\w/i.test(s) ? res+=alphabet[s] : res+=s,'')
+// }
 // 3) END===============================================================
 
 
