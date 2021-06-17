4ba669ad93274fe73c7bd83ca97a177d8408c030
Geordie King <geordie.king@gmail.com>
2021-06-18 00:57:28 +0100

    #1-problem kata for 18/Jun/2021

--- js/2021-06/2021-06-18.js
+++ js/2021-06/2021-06-18.js
@@ -8,6 +8,34 @@
 // TITLE: Training JS #3: Basic data types--String (8 kyu)
 // LINK:  https://www.codewars.com/kata/571edea4b625edcb51000d8e
 
+// fix this code
+// var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+// function Dad(){
+//   //select some variable to combine "Dad"
+//   return ?+?+?;
+// }
+// function Bee(){
+//   //select some variable to combine "Bee"
+//   return ?+?+?;
+// }
+// function banana(){
+//   //select some variable to combine "banana"
+//   return ?+?+?;
+// }
+
+// //answer some questions if you finished works above
+// function answer1(){
+//   //the answer should be "yes" or "no"
+//   return "";
+// }
+// function answer2(){
+//   //the answer should be "yes" or "no"
+//   return "";
+// }
+// function answer3(){
+//   //the answer should be "yes" or "no"
+//   return "";
+// }
 // 1) END===============================================================
 
 
