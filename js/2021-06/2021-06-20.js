e53dcc14c8c846676787f5b602006806eca87634
Geordie King <geordie.king@gmail.com>
2021-06-20 01:53:03 +0100

    #1v1 kata for 20/Jun/2021

--- js/2021-06/2021-06-20.js
+++ js/2021-06/2021-06-20.js
@@ -8,6 +8,18 @@
 // TITLE: Training JS #10: loop statement --for (8 kyu)
 // LINK:  https://www.codewars.com/kata/5721a78c283129e416000999
 
+// v1
+// function pickIt(arr){
+//   var odd=[],even=[];
+//   for (let i=0; i<arr.length; i++){
+//     if (arr[i]%2==1) { 
+//       odd.push(arr[i]) 
+//     } else {
+//       even.push(arr[i]) 
+//     }
+//   }
+//   return [odd,even];
+// }
 // 1) END===============================================================
 
 
