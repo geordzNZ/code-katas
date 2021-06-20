f029e9f539496094803b2a43d069114312741719
Geordie King <geordie.king@gmail.com>
2021-06-20 02:11:39 +0100

    #2 kata for 20/Jun/2021

--- js/2021-06/2021-06-20.js
+++ js/2021-06/2021-06-20.js
@@ -34,6 +34,17 @@
 // TITLE: Training JS #11: loop statement --break,continue (8 kyu)
 // LINK: https://www.codewars.com/kata/5721c189cdd71194c1000b9b/train/javascript 
 
+// v1
+// function grabDoll(dolls){
+//   var bag=[];
+//   for (let i=0; i<dolls.length; i++){
+//     if (dolls[i]!="Hello Kitty" && dolls[i]!="Barbie doll") { continue }
+//     bag.push(dolls[i])
+    
+//     if (bag.length==3) { break }
+//   }
+//   return bag;
+// }
 // 2) END===============================================================
 
 
