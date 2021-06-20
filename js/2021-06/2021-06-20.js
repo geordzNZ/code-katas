77503b70ce5ee903b384900fa72fb7991628c563
Geordie King <geordie.king@gmail.com>
2021-06-20 01:55:08 +0100

    #1v2 kata for 20/Jun/2021

--- js/2021-06/2021-06-20.js
+++ js/2021-06/2021-06-20.js
@@ -20,6 +20,13 @@
 //   }
 //   return [odd,even];
 // }
+
+// v2
+// function pickIt(arr){
+//   var odd=[],even=[];
+//   for (let i=0; i<arr.length; i++){ arr[i]%2 ? odd.push(arr[i]) : even.push(arr[i]) }
+//   return [odd,even];
+// }
 // 1) END===============================================================
 
 
